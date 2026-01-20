584c003aac817f471b835a250679bf1132000edb
Geetha Gangabattula <142496156+Geethaganbattula@users.noreply.github.com>
2026-01-20 20:37:34 +0530

    Add files via upload

---
+++ backend/app/models/subscriber.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String
+from app.database import Base
+
+class Subscriber(Base):
+    __tablename__ = "subscribers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
